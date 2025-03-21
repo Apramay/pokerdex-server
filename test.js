825535747f369dfae7fa06b1c3257da2f34ec709
Apramay <142184694+Apramay@users.noreply.github.com>
2025-03-21 17:46:57 -0400

    Update test.js

--- test.js
+++ test.js
@@ -16,7 +16,6 @@ const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 const rankValues = {
     "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14
 }; 
- const tables = new Map(); // Store game state for each table
 
 let playersWhoActed = new Set();
 // Function to create a new deck of cards
