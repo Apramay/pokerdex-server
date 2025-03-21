fffbec2b4ffb90591a0e97eb0285188be00cb9e6
Apramay <142184694+Apramay@users.noreply.github.com>
2025-03-21 11:02:24 -0400

    Update test.js

--- test.js
+++ test.js
@@ -16,18 +16,17 @@ const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 const rankValues = {
     "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14
 }; 
-// Game state variables
-// let players = [];
-// let tableCards = [];
-// let pot = 0;
-// let currentPlayerIndex = 0;
-// let deckForGame = [];
-// let currentBet = 0;
-// let dealerIndex = 0;
-// let round = 0;
-// let smallBlindAmount = 10;
-// let bigBlindAmount = 20;
-// let playersWhoActed = new Set();
+ let players = [];
+ let tableCards = [];
+let pot = 0;
+let currentPlayerIndex = 0;
+let deckForGame = [];
+let currentBet = 0;
+let dealerIndex = 0;
+let round = 0;
+let smallBlindAmount = 10;
+let bigBlindAmount = 20;
+let playersWhoActed = new Set();
 // Function to create a new deck of cards
 function createDeck() {
     let deck = [];
